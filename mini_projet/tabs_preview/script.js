0991a9b9dd35daf5cb3269032603433514651644
savvyh <7277@holbertonstudents.com>
2024-06-10 06:58:41 +0000

    Update the script of my tabs preview

--- mini_projet/tabs_preview/script.js
+++ mini_projet/tabs_preview/script.js
@@ -2,25 +2,37 @@ const tabOne = document.querySelector(".react");
 const tabTwo = document.querySelector(".vue");
 const tabThree = document.querySelector(".angular");
 const body = document.body;
-const item = document.querySelector(".item");
+const items = document.querySelectorAll(".item");
 const icon = document.querySelector(".big-icon");
-const active = document.querySelector(".active");
 
-function setColors(tab, bgColor, iconClass, infoColor){
+function setColors(bgColor, iconClass, infoColor){
   body.style.backgroundColor = bgColor;
-  icon.innerHTML = '<i class = "${iconClass}">';
-  for(let i = 0; i < item.length; i++ ){
-    item[i].style.color = infoColor;
-  }
+  icon.className = iconClass;
+  items.forEach(item => {
+    item.style.color = infoColor;
+  });
 }
 
 tabOne.addEventListener("click", () => {
-  setColors(tabOne, "#00d8ff", "fab fab-react", "#00d8ff");
+  setColors("#00d8ff", "fab fa-react", "#00d8ff");
   tabOne.classList.add("tabOne");
-  tabTwo.classList.remove("tabOne");
-  tabThree.classList.remove("tabOne");
+  tabTwo.classList.remove("tabTwo");
+  tabThree.classList.remove("tabThree");
 });
 
+tabTwo.addEventListener("click", () => {
+  setColors("#42b883", "fab fa-vuejs", "#42b883");
+  tabTwo.classList.add("tabTwo");
+  tabOne.classList.remove("tabOne");
+  tabThree.classList.remove("tabThree");
+});
 
-setColors(tabOne, "#00d8ff", "fab fab-react", "#00d8ff");
-tabOne.classList.add(tabOne);
+tabThree.addEventListener("click", () => {
+  setColors("#b52e31", "fab fa-angular", "#b52e31");
+  tabThree.classList.add("tabThree");
+  tabTwo.classList.remove("tabTwo");
+  tabOne.classList.remove("tabOne");
+});
+
+setColors("#00d8ff", "fab fa-react", "#00d8ff");
+tabOne.classList.add("tabOne");
